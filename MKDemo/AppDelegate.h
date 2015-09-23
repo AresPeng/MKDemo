7d48d8530c279e843a82b145b48772864a29db8a
dengjianping <ares@arespeng.local>
2015-09-23 13:52:43 +0800

    Initial Commit

---
+++ MKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKDemo
+//
+//  Created by ares on 15/9/23.
+//  Copyright © 2015年 AresPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MKDemo
+//
+//  Created by ares on 15/9/23.
+//  Copyright © 2015年 AresPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
